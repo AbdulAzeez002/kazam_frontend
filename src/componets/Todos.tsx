185decf416f47cea7179721bf6f9eae48ee1e5dc
Abdul Azeez <abdul.azeez@tisteps.co>
2024-04-24 09:53:27 +0530

    added responsiveness and loading

--- src/componets/Todos.tsx
+++ src/componets/Todos.tsx
@@ -3,21 +3,34 @@ import socketIOClient, { Socket } from "socket.io-client";
 import SingleTodo from "./SingleTodo";
 import { baseUrl, getAllTodos } from "../services/todo.servies";
 
+type Todo = {
+  _id: string;
+  todo: string;
+};
+
 const Todos = () => {
   const [sockets, setSockets] = useState<Socket | null>(null);
   const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState<string[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const socket = socketIOClient(baseUrl);
-    setSockets(socket);
-    socket.connect();
     getTodos();
+    conectToSocket();
 
     return () => {
       sockets?.disconnect();
     };
   }, []);
+
+  const conectToSocket = () => {
+    const socket = socketIOClient(baseUrl);
+    setSockets(socket);
+    socket.connect();
+    socket.on("todoadded", (newTodo: Todo) => {
+      setTodos((prevTodos) => [newTodo, ...prevTodos]); // Add the new todo to the list
+    });
+  };
 
   const addTodo = () => {
     if (todo?.trim()?.length == 0) {
@@ -25,25 +38,28 @@ const Todos = () => {
     }
 
     try {
-      sockets?.emit("addTodo", { data: todo });
-      setTodos((prev) => [todo, ...prev]);
+      sockets?.emit("add", { data: todo });
     } catch (error) {
       console.log(error, "error while adding todo");
+    } finally {
+      setTodo("");
     }
-    setTodo("");
   };
 
   const getTodos = async () => {
     try {
-      const response: { todos: string[] } = await getAllTodos();
+      setLoading(true);
+      const response: { todos: Todo[] } = await getAllTodos();
       setTodos(response?.todos);
     } catch (error) {
       console.log(error, "error");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div className="p-4 rounded-lg  sm:min-w-80 w-60 shadow-xl">
+    <div className="p-4 rounded-lg  sm:min-w-96 w-60 shadow-xl border-solid border-2 border-slate-200">
       <div className="flex pb-2 items-center">
         <div>
           <img src="notes-icon.png" alt="" height={40} width={40} />
@@ -82,9 +98,18 @@ const Todos = () => {
             </div>
           ) : (
             <div>
-              {todos?.map((todo) => (
-                <SingleTodo todo={todo} />
-              ))}
+              {loading ? (
+                <div className="flex justify-center items-center h-52">
+                  {" "}
+                  <div>Loading ..</div> .
+                </div>
+              ) : (
+                <div>
+                  {todos?.map((todo, index) => (
+                    <SingleTodo key={index} todo={todo.todo} />
+                  ))}
+                </div>
+              )}
             </div>
           )}
         </div>
